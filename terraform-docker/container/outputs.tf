2aa776a1396071f20387c82562476413f8be8f0c
Sangeet Prasad <ubuntu@ip-172-31-24-130.ec2.internal>
2021-10-22 14:19:18 +0000

    influxdb and nodered now output sorted

--- terraform-docker/container/outputs.tf
+++ terraform-docker/container/outputs.tf
@@ -1,11 +1,3 @@
-# output "ip-address-port" {
-#   value = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
-#   # value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address, i.ports[0].external])]
-
-#   description = "The IP address of the container"
-# }
-
-# output "container-name" {
-#   value       = docker_container.nodered_container.name
-#   description = "The Name of the container"
-# }
+output "application_access" {
+  value = { for x in docker_container.container[*] : x.name => join(":", [x.ip_address], x.ports[*]["external"]) }
+}
